a90a16d0f89862f37f33cbc104d2f507d908701b
LucasMiguel2007 <lucasmiguelg4533@gmail.com>
2026-08-09 22:00:56 -0300

    Segunda Alterações basicas

--- src/bot2.0.py
+++ src/bot2.0.py
@@ -5,7 +5,6 @@ from urllib.parse import quote
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pyautogui
 
 print("Navegador abrindo...")
 navegador = webdriver.Chrome()
